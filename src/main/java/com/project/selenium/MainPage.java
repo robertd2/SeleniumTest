de0ca1cc836c1804a776c7d181182a403a4f72b7
Robert Duda <robert_d2@o2.pl>
2016-12-30 11:42:46 +0100

    main page conflict

--- src/main/java/com/project/selenium/MainPage.java
+++ src/main/java/com/project/selenium/MainPage.java
@@ -1,25 +1,27 @@
 package com.project.selenium;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+
+import com.project.selenium.purchase.ClothingType;
+import com.project.selenium.purchase.SinglePurchase;
 
 public class MainPage implements IMainPage {
 	
 	private WebDriver driver;
 	
 	public MainPage(){
-		driver =Setup.getDriver();
+		driver = Setup.getDriver();
+		Setup.loadMainPage();		
 	}
 	
 	public void logInUser(User user) {
 		// TODO Auto-generated method stub
 	}
-	
 
-
-	public void buySomething(User user, String itemType, int numberOfItems) {
-		// TODO Auto-generated method stub
+	public void buySomething(User user, ClothingType itemType, int numberOfItems) {
+		SinglePurchase singlePurchase = new SinglePurchase(driver.findElement(By.id("block_top_menu")));
+		
 	}	
-
-	
 
 }
