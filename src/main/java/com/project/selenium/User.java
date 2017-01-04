002cb9b93649737df2bb976965183cb76522baff
Kostek Weber <konstanty.weber@capgemini.com>
2017-01-04 16:31:24 +0100

    test rejestracji

--- src/main/java/com/project/selenium/User.java
+++ src/main/java/com/project/selenium/User.java
@@ -1,12 +1,30 @@
 package com.project.selenium;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class User {
 
-	public static final int TYLKO_LOGIN_I_HASLO = 2;
+	public static final int TYLKO_EMAIL_I_HASLO = 2;
+	public static final int DANE_MINIMALNE = 13;
 
-	private String username;
 	private String email;
 	private String password;
+	private Sex gender;
+	private String firstName;
+	private String lastName;
+	private LocalDate birthDate;
+	private String address;
+	private String city;
+	private String state;
+	private String zipCode;
+	private String country;
+	private String mobile;
+	private String alias;
+
+	public enum Sex {
+		Male, Female;
+	}
 
 	public User() {
 
@@ -15,9 +33,25 @@ public class User {
 	public User(String[] stringTab) {
 		if (stringTab != null && stringTab.length > 1) {
 			switch (stringTab.length) {
-			case TYLKO_LOGIN_I_HASLO:
-				this.email = (String) stringTab[0];
-				this.password = (String) stringTab[1];
+			case TYLKO_EMAIL_I_HASLO:
+				email = (String) stringTab[0];
+				password = (String) stringTab[1];
+				break;
+			case DANE_MINIMALNE:
+				email = (String) stringTab[0];
+				password = (String) stringTab[1];
+				gender = stringTab[2] == "M" ? Sex.Male : Sex.Female;
+				firstName = stringTab[3];
+				lastName = stringTab[4];
+				DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+				birthDate = LocalDate.parse(stringTab[5], dtf);
+				address = stringTab[6];
+				city = stringTab[7];
+				state = stringTab[8];
+				zipCode = stringTab[9];
+				country = stringTab[10];
+				mobile = stringTab[11];
+				alias = stringTab[12];
 				break;
 			default:
 				System.out.println("Incorrect number of paramethers - " + stringTab.length);
@@ -29,14 +63,6 @@ public class User {
 	public User(String email, String password) {
 		this.email = email;
 		this.password = password;
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
 	}
 
 	public String getPassword() {
@@ -55,7 +81,103 @@ public class User {
 		this.password = password;
 	}
 
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
 	public String toString() {
 		return "User email: " + email + " pass: " + password;
+	}
+
+	public Sex getGender() {
+		return gender;
+	}
+
+	public void setGender(Sex gender) {
+		this.gender = gender;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public LocalDate getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(LocalDate birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getZipCode() {
+		return zipCode;
+	}
+
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	public String getAlias() {
+		return alias;
+	}
+
+	public void setAlias(String alias) {
+		this.alias = alias;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
 	}
 }
--- src/test/java/test/reader/FileReader.java
+++ src/test/java/test/reader/FileReader.java
@@ -1,90 +1,81 @@
 package test.reader;
 
-
-import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import com.project.selenium.User;
-
-public class FileReader<T>{
+public class FileReader<T> {
 
 	private Class<T> clazz;
 	IFileReaderStrategy fileReaderStrategy;
-	
+
 	String filePath;
 	List<String[]> fileContent;
-	boolean header=false;
-	
-	
-	
-	private FileReader(){
-		clazz=null;
+	boolean header = false;
+
+	private FileReader() {
+		clazz = null;
 	}
-	
-	public FileReader(String filePath,IFileReaderStrategy fileReaderStrategy, Class<T> clazz ){
+
+	public FileReader(String filePath, IFileReaderStrategy fileReaderStrategy, Class<T> clazz) {
 		this.fileReaderStrategy = fileReaderStrategy;
 		this.filePath = filePath;
-		this.fileContent=null;
-		this.clazz=clazz;
-	}
-	
-
-
-	public void loadFile(){
-		fileContent=readFile(filePath);
+		this.fileContent = null;
+		this.clazz = clazz;
 	}
 
-	List<String[]> readFile(String filePath ){
+	public void loadFile() {
+		fileContent = readFile(filePath);
+	}
+
+	List<String[]> readFile(String filePath) {
 		List<String[]> result;
 		result = fileReaderStrategy.readTextFile(filePath);
 		return result;
-		
+
 	}
-	
-	
-	public void listContent(){
-		System.out.println("Content of the file: "+filePath);
-		for(String[] elem : fileContent){
+
+	public void listContent() {
+		System.out.println("Content of the file: " + filePath);
+		for (String[] elem : fileContent) {
 			System.out.println(Arrays.toString(elem));
 		}
 	}
-	
-	public void listObjects(){
-		System.out.println("Objects of the file: "+filePath);
-		for(T elem : convertDataIntoListOfObjects()){
+
+	public void listObjects() {
+		System.out.println("Objects of the file: " + filePath);
+		for (T elem : convertDataIntoListOfObjects()) {
 			System.out.println(elem);
-		}		
+		}
 	}
-	
+
 	public List<T> convertDataIntoListOfObjects() {
-		
+
 		List<T> result = new ArrayList<T>();
-        
-		if(fileContent != null){
-			if(hasHeader()){
+
+		if (fileContent != null) {
+			if (hasHeader()) {
 				fileContent.remove(0);
 			}
-		for(String[] elem : fileContent){
-			try {
-				Object[] ot = new Object[] {elem};
-				result.add(clazz.getConstructor(String[].class).newInstance(ot));		
+			for (String[] elem : fileContent) {
+				try {
+					Object[] ot = new Object[] { elem };
+					result.add(clazz.getConstructor(String[].class).newInstance(ot));
 
-			} catch (InstantiationException | IllegalAccessException | IllegalArgumentException
-					| InvocationTargetException | NoSuchMethodException | SecurityException e) {
-				System.out.println("Problem with/no String[] constructor in a class :"+clazz.getName());
-				e.printStackTrace();
+				} catch (InstantiationException | IllegalAccessException | IllegalArgumentException
+						| InvocationTargetException | NoSuchMethodException | SecurityException e) {
+					System.out.println("Problem with/no String[] constructor in a class :" + clazz.getName());
+					e.printStackTrace();
+				}
 			}
-		}
-		}else{
+		} else {
 			System.out.println("Empty file or problem during reading");
 		}
 		return result;
-		
+
 	}
-	
+
 	public void setFileReaderStrategy(IFileReaderStrategy fileReaderStrategy) {
 		this.fileReaderStrategy = fileReaderStrategy;
 	}
@@ -96,6 +87,5 @@ public class FileReader<T>{
 	public void setHeader(boolean header) {
 		this.header = header;
 	}
-	
 
 }
